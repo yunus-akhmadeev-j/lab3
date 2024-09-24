c970207a178edfac9f5131c89930b979d6734301
Yunus Akhmadeev <akuma-sensei@inbox.ru>
2024-09-24 19:26:23 +0300

    Update render.js

--- src/framework/render.js
+++ src/framework/render.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 const RenderPosition = {
     BEFOREBEGIN: 'beforebegin',
     AFTERBEGIN: 'afterbegin',
@@ -22,29 +22,5 @@ const RenderPosition = {
   
   
   export {RenderPosition, createElement, render};
-=======
-const RenderPosition = {
-    BEFOREBEGIN: 'beforebegin',
-    AFTERBEGIN: 'afterbegin',
-    BEFOREEND: 'beforeend',
-    AFTEREND: 'afterend',
-  };
-  
-  
-  function createElement(template) {
-    const newElement = document.createElement('div');
-    newElement.innerHTML = template;
-  
-  
-    return newElement.firstElementChild;
-  }
-  
-  
-  function render(component, container, place = RenderPosition.BEFOREEND) {
-  container.insertAdjacentElement(place, component.getElement());
-  }
-  
-  
-  export {RenderPosition, createElement, render};
->>>>>>> cd04d837147e57f89912bdb4c2eb67d9ec6cff69
+
   
