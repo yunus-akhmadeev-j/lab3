06175a08a48f26477fb1cfdbd4a7a982146bbfb0
Yunus Akhmadeev <akuma-sensei@inbox.ru>
2024-09-24 19:24:34 +0300

    Update container.js

--- src/view/container.js
+++ src/view/container.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import {createElement} from '../framework/render.js';
 
 
@@ -31,37 +31,3 @@ export default class ContainerCont {
     this.element = null;
   }
 }
-=======
-import {createElement} from '../framework/render.js';
-
-
-function createContainerContTemplate() {
-    return (
-        `<div id="main">
-        
-        </div>`
-      );
-}
-
-
-export default class ContainerCont {
-  getTemplate() {
-    return createContainerContTemplate();
-  }
-
-
-  getElement() {
-    if (!this.element) {
-      this.element = createElement(this.getTemplate());
-    }
-
-
-    return this.element;
-  }
-
-
-  removeElement() {
-    this.element = null;
-  }
-}
->>>>>>> cd04d837147e57f89912bdb4c2eb67d9ec6cff69
