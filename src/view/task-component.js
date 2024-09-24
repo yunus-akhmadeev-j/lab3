cd04d837147e57f89912bdb4c2eb67d9ec6cff69
Yunus Akhmadeev <akuma-sensei@inbox.ru>
2024-09-24 19:14:48 +0300

    Add files via upload

---
+++ src/view/task-component.js
@@ -0,0 +1,36 @@
+import { createElement } from "../framework/render.js";
+function createTaskComponentTemplate(task) {
+    const {title, status} = task;
+    return (
+        `<section class="tasks">
+            <div class="task-body">
+           <ul><h3>${status}</h3>
+       <li class="task1">${title}</li>
+     
+       </ul>
+       
+        </div>
+        </section>`
+    );
+}
+export default class TaskComponent{
+
+    constructor({task}) {
+        this.task = task;
+    }
+
+
+    getTemplate() {
+    return createTaskComponentTemplate(this.task);
+    }
+
+    getElement() {
+    if (!this.element) {
+        this.element = createElement(this.getTemplate());
+    }
+    return this.element;
+    }
+    removeElement() {
+    this.element = null;
+    }   
+}
