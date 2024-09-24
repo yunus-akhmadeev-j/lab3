7d5c76ea645958d4bae6d71448c1983f3378e298
Yunus Akhmadeev <akuma-sensei@inbox.ru>
2024-09-24 19:23:59 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 
 import {render, RenderPosition} from './framework/render.js';
 
@@ -31,37 +31,3 @@ render(new ContainerCont(), containerContainer);
 tasksBoardPresenter.init();
 
 
-=======
-
-import {render, RenderPosition} from './framework/render.js';
-
-
-
-import HeaderCont from './view/header.js';
-import TaskAdder from './view/task-adder.js';
-import TaskBasket from './view/task-basket.js';
-import ContainerCont from './view/container.js';
-import TasksBoardPresenter from './presenter/tasks-board-presenter.js';
-import TasksModel from './model/task-model.js';
-
-
-
-const bodyContainer= document.querySelector('.header');
-const taskformContainer=document.querySelector('.newtask-add-button');
-const containerContainer=document.querySelector('.main');
-const taskbasketContainer=document.querySelector('.in-basket');
-const tasksModel = new TasksModel();
-const tasksBoardPresenter = new TasksBoardPresenter({boardContainer: tasksBoardContainer,  tasksModel,});
-
-
-
-render(new HeaderCont(), bodyContainer, RenderPosition.BEFOREBEGIN);
-render(new TaskAdder(), taskformContainer,  RenderPosition.BEFOREBEGIN);
-render(new ContainerCont(), containerContainer);
-render(new TaskBasket(), taskbasketContainer,  RenderPosition.BEFOREBEGIN);
-
-render(new ContainerCont(), containerContainer);
-tasksBoardPresenter.init();
-
-
->>>>>>> cd04d837147e57f89912bdb4c2eb67d9ec6cff69
