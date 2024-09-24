5292c6fdee4645741653f75ad2316e8a1f706bc6
Yunus Akhmadeev <akuma-sensei@inbox.ru>
2024-09-24 19:25:15 +0300

    Update task-basket.js

--- src/view/task-basket.js
+++ src/view/task-basket.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import {createElement} from '../framework/render.js';
 
 
@@ -35,41 +35,4 @@ export default class TaskBasket {
     this.element = null;
   }
 }
-=======
-import {createElement} from '../framework/render.js';
 
-
-function createTaskBasketTemplate() {
-    return (
-        `<div class="clear-button">
-            <button class="button-clear">
-                <a href="#">
-                    <p class="clear-button-text">Очистить</p>
-                </a>
-            </button>
-        </div> `
-      );
-}
-
-
-export default class TaskBasket {
-  getTemplate() {
-    return createTaskBasketTemplate();
-  }
-
-
-  getElement() {
-    if (!this.element) {
-      this.element = createElement(this.getTemplate());
-    }
-
-
-    return this.element;
-  }
-
-
-  removeElement() {
-    this.element = null;
-  }
-}
->>>>>>> cd04d837147e57f89912bdb4c2eb67d9ec6cff69
